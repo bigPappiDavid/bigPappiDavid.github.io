fd85082bbe8d7db7785c86fb590eee66cc5be8c1
David <david.munizmacias@pop.belmont.edu>
2019-06-04 03:02:00 -0500

    t

--- devices.js
+++ devices.js
@@ -1,15 +1,15 @@
-$(function() {
-
-
-   var people = [];
-
-   $.getJSON('data.json', function(data) {
-       $.each(data.person, function(i, f) {
-          var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
-           "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
-           $(tblRow).appendTo("#userdata tbody");
-     });
-
-   });
-
-});
+// $(function() {
+//
+//
+//    var people = [];
+//
+//    $.getJSON('data.json', function(data) {
+//        $.each(data.person, function(i, f) {
+//           var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
+//            "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
+//            $(tblRow).appendTo("#userdata tbody");
+//      });
+//
+//    });
+//
+// });
