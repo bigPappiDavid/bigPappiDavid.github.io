e19fd86bf8ba2e45fb1c481c643a3f03909acc03
David <david.munizmacias@pop.belmont.edu>
2019-06-04 02:55:30 -0500

    testing for java

--- devices.js
+++ devices.js
@@ -4,7 +4,7 @@ $(function() {
    var people = [];
 
    $.getJSON('data.json', function(data) {
-       $.each(data, function(i, f) {
+       $.each(data.person, function(i, f) {
           var tblRow = "<tr>" + "<td>" + f.manufacture + "</td>" +
            "<td>" + f.manufacture + "</td>" + "<td>" + f.manufacture + "</td>" + "<td>" + f.manufacture + "</td>" + "</tr>"
            $(tblRow).appendTo("#userdata tbody");
