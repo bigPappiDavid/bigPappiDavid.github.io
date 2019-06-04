76b52e3d29980767f4aeba94711a7f0b478df401
David <david.munizmacias@pop.belmont.edu>
2019-06-04 05:00:51 -0500

    test

--- js/devices.js
+++ js/devices.js
@@ -1,30 +1,30 @@
-$(function() {
-
-  // User clicks the "getData" button
-  $("#getData").click(function() {
-
-    // Put artistList element and JSON file location into a variable
-    var artistList = $("#artistList");
-    var url = "https://bigpappidavid.github.io/Devices/model.json";
-
-    // Get the JSON file
-    $.getJSON(url, function(data) {
-
-      // Put artist info into a variable
-      var artists = data.artists.map(function(item) {
-        return item.artistname + " (" + item.born + ")";
-      });
-
-      // Remove all child nodes (including text nodes)
-      artistList.empty();
-
-      // Format artists with HTML tags
-      if (artists.length) {
-        var content = "<li>" + artists.join("</li><li>") + "</li>";
-        var list = $("<ul>").html(content);
-        artistList.append(list);
-      }
-    });
-  });
-
-});
+// $(function() {
+//
+//   // User clicks the "getData" button
+//   $("#getData").click(function() {
+//
+//     // Put artistList element and JSON file location into a variable
+//     var artistList = $("#artistList");
+//     var url = "https://bigpappidavid.github.io/Devices/model.json";
+//
+//     // Get the JSON file
+//     $.getJSON(url, function(data) {
+//
+//       // Put artist info into a variable
+//       var artists = data.manufacture.map(function(item) {
+//         return item.manufacture + " (" + item.born + ")";
+//       });
+//
+//       // Remove all child nodes (including text nodes)
+//       artistList.empty();
+//
+//       // Format artists with HTML tags
+//       if (artists.length) {
+//         var content = "<li>" + artists.join("</li><li>") + "</li>";
+//         var list = $("<ul>").html(content);
+//         artistList.append(list);
+//       }
+//     });
+//   });
+//
+// });
